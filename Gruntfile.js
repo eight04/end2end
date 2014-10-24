4f9a7cf3a5618fa833aa9b72718fddf5208e672a
eight <eight04@gmail.com>
2014-10-24 16:51:46 +0800

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+		minified: {
+			files: {
+				"dist/dialog.min.js": "dist/dialog.js"
+			},
+			options: {
+				mangle: true,
+				compress: true,
+				sourceMap: true,
+			}
+		}
+    },
+	less: {
+		source: {
+			files: {
+				"dist/dialog.css": "dialog.less"
+			}
+		},
+		minified: {
+			files: {
+				"dist/dialog.min.css": "dialog.less"
+			},
+			options: {
+				compress: true,
+				sourceMap: true,
+				sourceMapFilename: "dist/dialog.min.css.map",
+				sourceMapURL: "dialog.min.css.map"
+			}
+		}
+	},
+	eslint: {
+		source: {
+			src: "dialog.js"
+		}
+	},
+	copy: {
+		dist: {
+			files: {
+				"dist/dialog.js": "dialog.js"
+			}
+		}
+	},
+	ngtemplates: {
+		template: {
+			src: "templates/**.html",
+			dest: "dist/dialog.js",
+			options: {
+				htmlmin: {
+					collapseWhitespace: true,
+					removeComments: true
+				},
+				module: "ezdialog",
+				append: true
+			}
+		}
+	}
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-angular-templates');
+
+  // Default task(s).
+  grunt.registerTask('default', ["eslint", "copy", "less", "ngtemplates", 'uglify']);
+
+};
