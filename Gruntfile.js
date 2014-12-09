dbfa8e72702332dc4f600ab4fec4da712166f3bb
eight <eight04@gmail.com>
2014-12-09 13:54:51 +0800

    Release v0.1.6

--- Gruntfile.js
+++ Gruntfile.js
@@ -92,7 +92,7 @@ module.exports = function(grunt) {
 		},
 		bump: {
 			options: {
-				files: ["package.json"],
+				files: ["package.json", "bower.json"],
 				updateConfigs: ["pkg"],
 				commitFiles: ["-a"],
 				push: false
