36db3f89f2fc888b60d38cfd042083ceb086a4d5
eight <eight04@gmail.com>
2015-01-05 17:30:24 +0800

    Add max-height directive

--- end2end.js
+++ end2end.js
@@ -1495,6 +1495,27 @@ angular.module(
 			}
 		};
 	};
+}).directive("fillViewHeight", function(){
+	var jar = [];
+
+	function setHeight(element) {
+		element.css("max-height", document.documentElement.clientHeight + "px");
+	}
+
+	angular.element(window).on("resize", function(){
+		var i;
+		for (i = 0; i < jar.length; i++) {
+			setHeight(jar[i]);
+		}
+	});
+
+	return {
+		restrict: "C",
+		link: function(scope, element){
+			jar.push(element);
+			setHeight(element);
+		}
+	};
 });
 
 })();
